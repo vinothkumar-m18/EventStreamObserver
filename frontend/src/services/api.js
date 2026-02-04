219c89704fc3bcbb6bf92ed9051a3a094ae69e6c
vinothkumar-m18 <vinothkumarm045@gmail.com>
2026-02-04 10:50:08 +0530

    update express server and mongodb server cloud url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL:'http://localhost:5000/api'
+    baseURL:'https://eventstreamobserver.onrender.com/api'
 });
 api.interceptors.request.use(config =>{
     const token = localStorage.getItem('token');
