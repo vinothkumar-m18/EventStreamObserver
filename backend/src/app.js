48ddf654d2ca79edb91ac6914db5e2b3155814be
vinoth kumar <vinothkumarm4567@gmail.com>
2026-09-11 10:36:57 +0530

    add localhost to cors list

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ import protect from './middlewares/protect.js';
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin:['https://eventstreamobserver-1.onrender.com']
+    origin:['https://eventstreamobserver-1.onrender.com', 'http://localhost:5173']
 }));
 app.use(morgan('dev'));
 app.use('/api/webhook', webhookRoutes);
