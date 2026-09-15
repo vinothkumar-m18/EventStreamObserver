d7b23bd2c951fb1d63ec67bdacb0028779334c5d
vinoth kumar <vinothkumarm4567@gmail.com>
2026-09-15 09:56:53 +0530

    update socket server url inside socket.io client

--- frontend/src/socketClient.js
+++ frontend/src/socketClient.js
@@ -1,5 +1,5 @@
 import {io} from 'socket.io-client';
-const socket = io('https://eventstreamobserver.onrender.com', {
+const socket = io('https://eventstreamobserver-130j.onrender.com', {
     transports:['websocket'],
     autoConnect:true
 });
