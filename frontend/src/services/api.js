1d1d7435dd611b83272e412048cf002967019b74
vinoth kumar <vinothkumarm4567@gmail.com>
2026-09-22 09:54:26 +0530

    updated the new backend render url inside api base url config

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL:'https://eventstreamobserver-130j.onrender.com/api'
+    baseURL:'https://backend-ajsh.onrender.com'
 });
 api.interceptors.request.use(config =>{
     const token = localStorage.getItem('token');
