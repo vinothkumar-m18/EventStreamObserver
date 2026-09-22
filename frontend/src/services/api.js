7202a377d1568ea9982291fe074e93a0e02fb2b1
vinoth kumar <vinothkumarm4567@gmail.com>
2026-09-22 10:50:03 +0530

    add 'api' to the end of the axios base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL:'https://backend-ajsh.onrender.com'
+    baseURL:'https://backend-ajsh.onrender.com/api'
 });
 api.interceptors.request.use(config =>{
     const token = localStorage.getItem('token');
